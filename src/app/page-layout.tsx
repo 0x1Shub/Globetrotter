9b806781bf4b12a960fa40ab9a3f6755cf3e8f59
0x1Shub <shubhamyeram13@gmail.com>
2025-01-08 15:16:13 +0530

    Client trip search results page

--- src/app/page-layout.tsx
+++ src/app/page-layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const PageLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className='relative flex flex-col' id='app-container'>
-        <main className='flex flex-col relative '>
+        <main className='flex flex-col relative'>
             <Navbar />
             <section className='h-full flex-1'>{children}</section>
             <Footer />
