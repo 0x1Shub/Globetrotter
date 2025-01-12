b58763dd6e593f515c42841b520c039fa7258116
0x1Shub <shubhamyeram13@gmail.com>
2025-01-12 19:27:21 +0530

    Client Logout

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,17 @@
+import { cookies } from 'next/headers'
+import React from 'react'
+import Actions from './actions';
+
+const Logout = () => {
+
+    async function deleteCookie() {
+        "use server";
+        (await cookies()).delete("access_token");
+    }
+
+  return (
+    <Actions deleteCookie={deleteCookie} />
+  )
+}
+
+export default Logout
